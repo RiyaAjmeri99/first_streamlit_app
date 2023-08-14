e27581d7dc874f683c2ffa0850a9acf42b0ae392
RiyaAjmeri99 <138483949+RiyaAjmeri99@users.noreply.github.com>
2023-08-14 13:21:32 +0530

    Update streamlit_app.py
    entered text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,14 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+#Entered the text box for taking input from user
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #This is will return the output as it is
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
 
 # The below statement one will normalize the data
